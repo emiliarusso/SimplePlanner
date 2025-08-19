53a8e1de80138bcee212cb16021955a60f57e78f
emiliarusso <russo.1966203@studenti.uniroma1.it>
2025-08-19 21:54:58 +0200

    created workspace

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/lattinone/SimplePlanner/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/lattinone/SimplePlanner/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/lattinone/SimplePlanner/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/lattinone/SimplePlanner/src:$ROS_PACKAGE_PATH"
