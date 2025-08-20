a5778a3b63bacf1db06e992b7c11d34a343e22c2
emiliarusso <russo.1966203@studenti.uniroma1.it>
2025-08-20 09:21:39 +0200

    simple planner package added

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,7 +7,4 @@
 # new environment variables
 
 # modified environment variables
-export CMAKE_PREFIX_PATH="/home/lattinone/SimplePlanner/devel:$CMAKE_PREFIX_PATH"
 export PWD='/home/lattinone/SimplePlanner/build'
-export ROSLISP_PACKAGE_DIRECTORIES='/home/lattinone/SimplePlanner/devel/share/common-lisp'
-export ROS_PACKAGE_PATH="/home/lattinone/SimplePlanner/src:$ROS_PACKAGE_PATH"
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/lattinone/SimplePlanner/build/gtest/cmake_install.cmake")
+  include("/home/lattinone/SimplePlanner/build/simple_planner/cmake_install.cmake")
 
 endif()
 
