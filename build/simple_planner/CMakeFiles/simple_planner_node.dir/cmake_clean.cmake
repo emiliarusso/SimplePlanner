fd341d1374035a62121a4bb5a67a868542392a71
emiliarusso <russo.1966203@studenti.uniroma1.it>
2025-08-27 15:50:13 +0200

    fix move_base error

---
+++ build/simple_planner/CMakeFiles/simple_planner_node.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "/home/lattinone/SimplePlanner/devel/lib/simple_planner/simple_planner_node"
+  "/home/lattinone/SimplePlanner/devel/lib/simple_planner/simple_planner_node.pdb"
+  "CMakeFiles/simple_planner_node.dir/src/node_ros1.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple_planner_node.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
